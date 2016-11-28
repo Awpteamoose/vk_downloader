b15023847debb36950a3953fffea6b96db984c95
Awpteamoose <awpteamoose@gmail.com>
2016-11-28 21:11:11 +0200

    user posts also ok

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var fs = require('fs-extended');
 var request = require('request');
 var sanitize = require('sanitize-filename');
 var wallpost_url = process.argv[2];
-var wallpost_id = wallpost_url.slice(wallpost_url.indexOf("wall-") + "wall".length);
+var wallpost_id = wallpost_url.slice(wallpost_url.indexOf("wall") + "wall".length);
 
 request(`https://api.vk.com/method/wall.getById?posts=${wallpost_id}`, (error, response, body) => {
 	if (!error && response.statusCode == 200) {
@@ -13,6 +13,7 @@ request(`https://api.vk.com/method/wall.getById?posts=${wallpost_id}`, (error, r
 			if (attach.type !== "audio") return;
 			var audio = attach.audio;
 			var filename = sanitize(`${audio.artist} - ${audio.title}`, { "replacement": "~" });
+			console.log(filename);
 			filename = "out\\" + filename + ".mp3";
 			fs.ensureFileSync(filename);
 			request(`${audio.url}`).pipe(fs.createWriteStream(filename));
